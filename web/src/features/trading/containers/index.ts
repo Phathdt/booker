9491161646c5ba09a4330758fcec002d4036e867
Phathdt <phathdt379@gmail.com>
2026-04-12 23:30:56 +0700

    fix: remove stale TRADING_PAIRS re-export from containers index

--- web/src/features/trading/containers/index.ts
+++ web/src/features/trading/containers/index.ts
@@ -1,4 +1,4 @@
-export { PairSelector, TRADING_PAIRS } from "./pair-selector";
+export { PairSelector } from "./pair-selector";
 export { OrderForm } from "./order-form";
 export { OpenOrders } from "./open-orders";
 export { OrderBook } from "./order-book";
